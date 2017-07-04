47d790f06e929b41c2b7934fd8214b2b30526478
Erter <mrmuff@icloud.com>
2017-07-04 15:11:41 -0700

    Updated main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,15 @@ resource "aws_launch_configuration" "example" {
   image_id = "ami-327f5352"
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_security_group" "instance" {
@@ -34,6 +43,11 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
 }
 
 variable "server_port" {
@@ -45,3 +59,42 @@ output "public_ip" {
   value = "${aws_instance.example.public_ip}"
 }
 */
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones        = ["${data.aws_availability_zones.all.names}"]
+  min_size = 2
+  max_size = 3
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
