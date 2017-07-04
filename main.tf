1ec1c707b990b19f23a4e96c6e06ae9121f92e3e
Erter <mrmuff@icloud.com>
2017-07-04 16:23:48 -0700

    Working ASG and ELB in us-west-1

--- main.tf
+++ main.tf
@@ -20,9 +20,10 @@ resource "aws_instance" "example" {
 */
 
 resource "aws_launch_configuration" "example" {
-  image_id = "ami-327f5352"
+  image_id = "ami-73f7da13"
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.instance.id}"]
+  key_name = "NorthernCaliforniaKeyPair"
 
   user_data = <<-EOF
               #!/bin/bash
@@ -44,6 +45,13 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = "${var.ssh_port}"
+    to_port = "${var.ssh_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   lifecycle {
     create_before_destroy = true
   }
@@ -54,6 +62,11 @@ variable "server_port" {
   description = "Port web server listens on"
   default = 8080
 }
+
+variable "ssh_port" {
+  description = "ssh port"
+  default = 22
+}
 /*
 output "public_ip" {
   value = "${aws_instance.example.public_ip}"
@@ -63,6 +76,10 @@ output "public_ip" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones        = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
   min_size = 2
   max_size = 3
 
@@ -86,6 +103,15 @@ resource "aws_elb" "example" {
     instance_port = "${var.server_port}"
     instance_protocol = "http"
   }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+
 }
 
 resource "aws_security_group" "elb" {
@@ -97,4 +123,15 @@ resource "aws_security_group" "elb" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
